d55d70cfcc8154b8e63b62950dac2d2f94d55f4c
Pcoch <74082459+pcoch@users.noreply.github.com>
2022-05-29 10:01:30 +1000

    AsyncAwait
    Refactored from Promise to Async Await syntax

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ window.onload = () => {
 
     const img = document.querySelector('img');    
 
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XTwdhedRaeuU3hRmyS7YeEcAmR62Y8EH&s=${searchQuery}`, {mode: 'cors'})
-    .then(function(response) {
-    return response.json();
-        })
-    .then(function(response) {
-    img.src = (response.data.images.original.url);
-    const loader = document.querySelector('.loader');
-    loader.classList.add('hide');
-        }).catch((error) => {
-            console.error('Error:', error);
-          });
+
+    async function getGiph() {
+
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XTwdhedRaeuU3hRmyS7YeEcAmR62Y8EH&s=${searchQuery}`, {mode: 'cors'});
+        const giphData = await response.json();
+        img.src = (giphData.data.images.original.url);
+        const loader = document.querySelector('.loader');
+        loader.classList.add('hide');
+    }
+    getGiph();
 
 //on btn click, show new image
 const Btn = document.querySelector('.button');
@@ -25,52 +24,35 @@ const searchBtn = document.querySelector('.search-btn');
 searchBtn.addEventListener('click', searchGiph);
 };
 
-//show new image, on click
-const generateGiph = () => {
-    const img = document.querySelector('img');    
-    img.src = '';
+//show new image, on click   
+    async function generateGiph() {
 
-    let searchQuery = document.getElementById('search').value;
-
-    const loader = document.querySelector('.loader');
-    loader.classList.remove('hide');
-
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XTwdhedRaeuU3hRmyS7YeEcAmR62Y8EH&s=${searchQuery}`, {mode: 'cors'})
-    .then(function(response) {
-    return response.json();
-        })
-    .then(function(response) {
-    img.src = (response.data.images.original.url);
-    loader.classList.add('hide');
-        }).catch((error) => {
-            console.error('Error:', error);
-          });
-};
-
+        const img = document.querySelector('img');    
+        img.src = '';
+    
+        let searchQuery = document.getElementById('search').value;
+    
+        const loader = document.querySelector('.loader');
+        loader.classList.remove('hide');
+    
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XTwdhedRaeuU3hRmyS7YeEcAmR62Y8EH&s=${searchQuery}`, {mode: 'cors'});
+        const giphData = await response.json();
+        img.src = (giphData.data.images.original.url);
+        loader.classList.add('hide');
+    };
 
 //show new image, on search
-const searchGiph = () => {
+    async function searchGiph () {
+        let searchQuery = document.getElementById('search').value;
 
-    let searchQuery = document.getElementById('search').value;
-
-    const img = document.querySelector('img');    
-    img.src = '';
-
-    const loader = document.querySelector('.loader');
-    loader.classList.remove('hide');
-
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XTwdhedRaeuU3hRmyS7YeEcAmR62Y8EH&s=${searchQuery}`, {mode: 'cors'})
-    .then(function(response) {
-    return response.json();
-        })
-    .then(function(response) {
-    img.src = (response.data.images.original.url);
-    loader.classList.add('hide');
-        }).catch((error) => {
-            console.error('Error:', error);
-          });
-};
-
-
-
-
+        const img = document.querySelector('img');    
+        img.src = '';
+    
+        const loader = document.querySelector('.loader');
+        loader.classList.remove('hide');
+    
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XTwdhedRaeuU3hRmyS7YeEcAmR62Y8EH&s=${searchQuery}`, {mode: 'cors'});
+        const giphData = await response.json();
+        img.src = (giphData.data.images.original.url);
+        loader.classList.add('hide');        
+    };
